be1404be30a520edd3af7b97943f2e1d6162e294
steve303 <susc@colorado.edu>
2021-04-15 21:41:57 +0000

    init commit

--- part_a.py
+++ part_a.py
@@ -13,23 +13,38 @@ def get_connected_components(graphframe):
     # For example, [[a_1, a_2, ...], [b_1, b_2, ...], ...]
     # then a_1, a_2, ..., a_n lie in the same component,
     # b_1, b2, ..., b_m lie in the same component, etc
-    return [[]]
-
+    dict01 = {}
+    df = graphframe.connectedComponents()
+    data = df.collect()
+    for val in data:
+        if val[1] not in dict01:
+            dict01[val[1]] = [val[0]]
+        else:
+            dict01[val[1]].append(val[0])
+    #print(dict01)        
+    results = []
+    vertices = dict01.values()
+    for v in vertices:
+        results.append(v)
+    return results
 
 if __name__ == "__main__":
     vertex_list = []
     edge_list = []
     with open('dataset/graph.data') as f:  # Do not modify
         for line in f:
-            src = None  # TODO: Parse src from line
-            dst_list = []  # TODO: Parse dst_list from line
+            data = line.strip().split(' ')
+            src = data[0]  # TODO: Parse src from line
+            dst_list = data[1:]  # TODO: Parse dst_list from line
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create vertices dataframe
-    edges = spark.createDataFrame(...)  # TODO: Create edges dataframe
+    vertices = spark.createDataFrame(vertex_list, ['id'])  # TODO: Create vertices dataframe
+    edges = spark.createDataFrame(edge_list, ['src', 'dst'])  # TODO: Create edges dataframe
 
     g = GraphFrame(vertices, edges)
+    #print(g)
+    #display(g.vertices)
     sc.setCheckpointDir("/tmp/connected-components")
 
     result = get_connected_components(g)
